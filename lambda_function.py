9b0d543b767e7472d3e58f91942fc4b8dbc0ff4c
José Márquez <jose.marquez@gosocket.net>
2023-01-28 17:53:25 -0500

    first commit

--- lambda_function.py
+++ lambda_function.py
@@ -6,3 +6,8 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hola Lambda!')
     }
+# declare a lambda function
+greet = lambda : print('Hello World')
+
+# call lambda function
+greet()
